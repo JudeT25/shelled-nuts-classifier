937c4f987d55dce9648fa407b52e40a396afc171
JudeT25 <107264802+JudeT25@users.noreply.github.com>
2023-05-15 22:53:39 -0400

    Update app.py

--- app.py
+++ app.py
@@ -39,4 +39,5 @@ def predict():
         #except:
            # return jsonify({'error': 'error during prediction'}
 if __name__ == "__main__":
-    app.run(debug=False, host='0.0.0.0')
+    if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True)
